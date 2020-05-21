f1898edb827554c58f7d85b825270e548b6bd829
IdionKim <ksundong@gmail.com>
2020-05-21 18:39:51 +0900

    [BE #7] feat: room 검색 객체 생성

--- BE/src/main/java/kr/codesquad/airbnb11/controller/RoomRestController.java
+++ BE/src/main/java/kr/codesquad/airbnb11/controller/RoomRestController.java
@@ -1,10 +1,11 @@
 package kr.codesquad.airbnb11.controller;
 
+import kr.codesquad.airbnb11.controller.request.SearchRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/rooms")
@@ -13,10 +14,9 @@ public class RoomRestController {
 
   private static final Logger log = LoggerFactory.getLogger(RoomRestController.class);
 
-  // TODO: Request 객체 정의하기
   @GetMapping("/search")
-  public void searchRooms(@RequestParam(required = false) String aaa) {
-    log.debug("hello");
+  public void searchRooms(@RequestBody(required = false) SearchRequest searchRequest) {
+    log.debug("searchRequest: {}", searchRequest);
   }
 
 }
