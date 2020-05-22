51097c09507f0fc9c846b71328fd71e957e6e005
Han <quaint3@gmail.com>
2020-05-22 22:13:33 +0900

    [BE #29] fix : id type 수정

--- BE/src/main/java/kr/codesquad/airbnb11/domain/review/ReviewRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/review/ReviewRepository.java
@@ -2,6 +2,6 @@ package kr.codesquad.airbnb11.domain.review;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ReviewRepository extends CrudRepository<Review, Long> {
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
 
 }
