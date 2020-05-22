980f5297a3ba1c32a6972a27bf6369f93f63e55a
IdionKim <ksundong@gmail.com>
2020-05-22 16:10:43 +0900

    [BE #29] feat: Room Repository인터페이스 생성
    Aggregate root인 Room의 Repository를 추가합니다.

---
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/room/RoomRepository.java
@@ -0,0 +1,7 @@
+package kr.codesquad.airbnb11.domain.room;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+
+}
