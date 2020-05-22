9c6f5642511f3dff851426764113683bd8fdfc4c
Han <quaint3@gmail.com>
2020-05-22 20:18:10 +0900

    [BE #29] feature : Review 객체 생성
    일반적으로 Review를 생성할 때는 id가 필요없으니,
    생성자를 둘로 나눴음.

--- BE/src/main/java/kr/codesquad/airbnb11/domain/review/Review.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/review/Review.java
@@ -1,5 +1,62 @@
 package kr.codesquad.airbnb11.domain.review;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+
 public class Review {
 
+  @Id
+  private Integer id;
+  private Integer rating;
+  private String content;
+  private Integer roomId;
+  private Integer guestId;
+
+  public Review(Integer rating, String content, Integer roomId, Integer guestId) {
+    this.rating = rating;
+    this.content = content;
+    this.roomId = roomId;
+    this.guestId = guestId;
+  }
+
+  @PersistenceConstructor
+  private Review(Integer id, Integer rating, String content, Integer roomId, Integer guestId) {
+    this.id = id;
+    this.rating = rating;
+    this.content = content;
+    this.roomId = roomId;
+    this.guestId = guestId;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public Integer getRating() {
+    return rating;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public Integer getRoomId() {
+    return roomId;
+  }
+
+  public Integer getGuestId() {
+    return guestId;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this)
+        .append("id", id)
+        .append("rating", rating)
+        .append("content", content)
+        .append("roomId", roomId)
+        .append("guestId", guestId)
+        .toString();
+  }
 }
