d585b70c830af21ceac082d0ae5e1e902052130f
IdionKim <ksundong@gmail.com>
2020-05-24 15:28:10 +0900

    [BE #6] fix: auto-deploy를 위한 workflow 경로가 잘못되어 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/testapp
+REPOSITORY=/home/ubuntu/testapp/BE
 cd $REPOSITORY
 
 APP_NAME=airbnb11
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=test $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
