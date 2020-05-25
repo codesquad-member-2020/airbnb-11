43195641e542bc3be67ecbdd4b69c2f88bf1c407
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-26 01:07:03 +0900

    [FE #18] feat: TextButton 컴포넌트 작성.
    Title >
    * TextButton 컴포넌트 작성
    
    Description >
    * Main Page 의 Header 에서 사용되는 TextButton 컴포넌트 작성

--- FE/src/components/Main/Header/HeaderNavigation/TextButton/TextButton.jsx
+++ FE/src/components/Main/Header/HeaderNavigation/TextButton/TextButton.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components'
 
-function TextButton() {
+const S = {};
+S.TextButton = styled.a`
+  display: inline-block;
+  position: relative;
+  background-color: transparent;
+  border-radius: 22px;
+  line-height: 18px;
+  font-size: 14px;
+  color: #222222;
+  font-weight: bold;
+  top: 50%;
+  transform: translateY(-50%);
 
+  &:hover {
+    color: black;
+    background-color: #f7f7f7;
+  }
+`;
+
+S.Title = styled.div`
+  position: relative;
+  padding: 12px;
+`;
+
+function TextButton(props) {
   return (
-      <>
-        <h1>TextButton</h1>
-      </>
+    <S.TextButton href={props.href}>
+      <S.Title>{props.title}</S.Title>
+    </S.TextButton>
   );
 }
 
