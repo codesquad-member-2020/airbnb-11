c36923e074e22d45dda5ff0a6147cd49da70b02b
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-25 23:55:06 +0900

    [FE #18] modify: style 을 image -> div, 중앙 정렬되도록 수정
    Title >
    * style 을 image -> div, 중앙 정렬되도록 수정
    
    Description >
    * 컴포넌트의 style 을 image 에서 div 로 수정
    * vertical alignment 가 부모 컴포넌트의 중앙이 되도록 수정

--- FE/src/components/Main/Header/Logo/Logo.jsx
+++ FE/src/components/Main/Header/Logo/Logo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const StyledLogo = styled.img`
+const StyledLogo = styled.div`
   position: relative;
+  float: left;
   width: 118px;
   height: 64px;
   background-size: 100% 100%;
+  background-image: url(${props => props.src});
+  top: 50%;
+  transform: translateY(-50%);
 `;
 
 function Logo(props) {
