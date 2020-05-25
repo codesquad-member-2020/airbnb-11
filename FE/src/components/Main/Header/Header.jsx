56f9ce802d0ba6d1187f947cfbfd533d535acfc6
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-26 01:13:20 +0900

    [FE #18] feat: Header 컴포넌트 작성
    Title >
    * Header 컴포넌트 작성
    
    Description >
    * Main Page 에서 버튼들을 포함하고 있는 HeaderNavigation 과 Logo 를
    렌더링하는 컴포넌트 작성

--- FE/src/components/Main/Header/Header.jsx
+++ FE/src/components/Main/Header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components'
+
+import Logo from './Logo/Logo'
+import HeaderNavigation from './HeaderNavigation/HeaderNavigation'
+import mainpageConstants from '../../../constants/mainpage'
+
+const S = {}
+S.Header = styled.div`
+  position: relative;
+  width: 1600px;
+  height: 80px;
+  color: black;
+  font-size: 24px;
+  margin: 0 auto;
+`;
 
 function Header() {
 
   return (
-      <>
-        <h1>Header</h1>
-      </>
+      <S.Header>
+        <Logo src={mainpageConstants.logoImageSrc} />
+        <HeaderNavigation></HeaderNavigation>
+      </S.Header>
   );
 }
 
