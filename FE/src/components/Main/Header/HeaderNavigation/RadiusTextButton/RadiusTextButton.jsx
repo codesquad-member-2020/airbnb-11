5e302cde375c16a32b216a6d912c0af4145870fe
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-26 01:07:51 +0900

    [FE #18] feat: RadiusTextButton 컴포넌트 작성
    Title >
    * RadiusTextButton 컴포넌트 작성
    
    Description >
    * Main Page 의 Header 에서 사용되는 (로그인) RadiusTextButton 컴포넌트 작성

--- FE/src/components/Main/Header/HeaderNavigation/RadiusTextButton/RadiusTextButton.jsx
+++ FE/src/components/Main/Header/HeaderNavigation/RadiusTextButton/RadiusTextButton.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from 'styled-components'
 
-function RadiusTextButton() {
+const S = {};
+S.TextButton = styled.a`
+  display: inline-block;
+  position: relative;
+  background-color: transparent;
+  border: 1px solid #dadfe0;
+  border-radius: 22px;
+  line-height: 18px;
+  font-size: 14px;
+  color: #222222;
+  font-weight: bold;
+  top: 50%;
+  transform: translateY(-50%);
 
+  &:hover {
+    color: black;
+    box-shadow: 1px 1px 1px 1px gray;
+  }
+`;
+
+S.Title = styled.div`
+  position: relative;
+  padding: 12px;
+`;
+
+function RadiusTextButton(props) {
   return (
-      <>
-        <h1>RadiusTextButton</h1>
-      </>
+    <S.TextButton href={props.href}>
+      <S.Title>{props.title}</S.Title>
+    </S.TextButton>
   );
 }
 
