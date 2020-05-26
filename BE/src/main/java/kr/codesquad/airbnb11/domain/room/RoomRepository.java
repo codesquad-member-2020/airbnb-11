721924ee29a93a82d4d234eccf65b95dbd741d43
Han <quaint3@gmail.com>
2020-05-26 16:06:33 +0900

    [BE #44] feat : Person Count method 추가
    최대 인원수에 따라 검색하는 기능 추가

--- BE/src/main/java/kr/codesquad/airbnb11/domain/room/RoomRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/room/RoomRepository.java
@@ -1,5 +1,11 @@
 package kr.codesquad.airbnb11.domain.room;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface RoomRepository extends CrudRepository<Room, Integer> {}
+public interface RoomRepository extends PagingAndSortingRepository<Room, Integer> {
+
+  @Transactional(readOnly = true)
+  List<Room> findAllByMaxPersonCountIsLessThanEqual(int maxPersonCount);
+}
