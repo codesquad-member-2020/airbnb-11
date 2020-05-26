28fce816016b7c074c1d9348c87ab87c894fd85d
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-26 20:45:24 +0900

    [FE] chore: polyfill 추가
    Title >
    * polyfill 추가
    
    Description >
    * async/await 를 위한 polyfill 추가

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = env => ({
   mode: env.mode,
-  entry: './src/index.jsx',
+  entry:  ['@babel/polyfill', './src/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
