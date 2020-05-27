c8bc86de7d8e690d577a1e1f222de43e52ec5206
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-27 16:02:28 +0900

    [FE #18] Modify: MainPage 에서 SearchNavigation 렌더링
    Title >
    * MainPage 에서 SearchNavigation 렌더링
    
    Description >
    * MainPage 에서 체크인/체크아웃, 인원, 검색 버튼을 포함하고 있는
    SearchNavigation 이 렌더링 되도록 수정

--- FE/src/components/Main/Main.jsx
+++ FE/src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header/Header'
+import SearchNavigation from './SearchNavigation/SearchNavigation'
 
 function Main() {
-
   return (
       <>
         <Header />
+        <SearchNavigation />
+        <img src="http://dev-angelo.dlinkddns.com/bg_1.png"></img>
+        <img src="http://dev-angelo.dlinkddns.com/bg_2.png"></img>
       </>
   );
 }
