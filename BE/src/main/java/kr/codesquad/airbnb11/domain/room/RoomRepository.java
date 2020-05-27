59bf48ccb1df39ad03c3dd298965770dd7fee904
Han <quaint3@gmail.com>
2020-05-27 11:13:19 +0900

    [BE #44] fix : transaction annotation 삭제, repository 메서드 오류 수정
    불필요한 어노테이션 삭제(조회 쿼리에는 트랜잭션이 필요없을까?)
    최소 인원 수 이상으로 검색해야하니까 메서드 수정

--- BE/src/main/java/kr/codesquad/airbnb11/domain/room/RoomRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/room/RoomRepository.java
@@ -2,10 +2,8 @@ package kr.codesquad.airbnb11.domain.room;
 
 import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface RoomRepository extends PagingAndSortingRepository<Room, Integer> {
 
-  @Transactional(readOnly = true)
-  List<Room> findAllByMaxPersonCountIsLessThanEqual(int maxPersonCount);
+  List<Room> findAllByMaxPersonCountIsGreaterThanEqual(int minPersonCount);
 }
