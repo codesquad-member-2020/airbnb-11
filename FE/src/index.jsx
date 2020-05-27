af9b96cde13e2d244dcbe051cadbae907d974705
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-27 17:24:57 +0900

    [FE] chore: rootReducer import 경로 수정
    Title >
    * rootReducer import 경로 수정
    
    Description >
    * reducer 관련 내용이 statement 이하 폴더로 이동하였기 때문에 경로 수정

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './statement/reducers/rootReducer'
 import '../lib/style/reset.css'
 import App from './App';
 
