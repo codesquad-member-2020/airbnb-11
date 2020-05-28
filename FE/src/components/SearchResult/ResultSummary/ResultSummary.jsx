35fe48b27c424f1efa0f0df49062607099c8acb8
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-29 01:29:20 +0900

    [FE #16] feat: ResultSummary 컴포넌트 작성
    Title >
    * ResultSummary 컴포넌트 작성
    
    Description >
    * 숙소 카운트, 체크인/체크아웃 날짜, 투숙 인원등을 나타내는 컴포넌트
    작성

---
+++ FE/src/components/SearchResult/ResultSummary/ResultSummary.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+
+const S = {};
+
+S.ResultSummary = styled.div`
+  display: relative;
+  height: 26px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-size: 14px;
+`;
+
+function ResultSummary(props) {
+  return (
+      <S.ResultSummary>
+        {props.summary}
+      </S.ResultSummary>
+  );
+}
+
+export default ResultSummary;
