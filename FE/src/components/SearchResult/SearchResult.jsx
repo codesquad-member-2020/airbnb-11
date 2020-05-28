8b659896d2a65ed45e4fda7be9a7569749e16b02
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-29 03:19:00 +0900

    [FE #16] SearchResult 페이지 작성 (In Progress)
    Title >
    * SearchResult 페이지 작성
    
    Description >
    * 검색 결과를 나타내는 페이지 작성
    * Todo 1: 하드코딩 되어있는 부분 제거 후
      fetch 에 의한 데이터를 렌더링 하도록 수정
    * Todo 2: 요금 범위 선택 기능 추가
    * Todo 3: 호텔 클릭 시 결제 모달이 나타나는 기능 개발

--- FE/src/components/SearchResult/SearchResult.jsx
+++ FE/src/components/SearchResult/SearchResult.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React from "react";
+import styled, { css } from "styled-components";
+import AccomodationCard from "Components/SearchResult/AccomodationCard/AccomodationCard";
+import Caution from "Components/SearchResult/Caution/Caution";
+import ResultSummary from "Components/SearchResult/ResultSummary/ResultSummary";
+import Header from "Components/SearchResult/Header/Header";
+
+const S = {};
+
+S.SearchResult = styled.div`
+  display: relative;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+S.SearchResultContentWrap = styled.div`
+  display: relative;
+  width: 1560px;
+  margin: 0 auto;
+`;
+
+S.AccomodationCardGrid = styled.div`
+  display: grid;
+  grid-template-rows: 300px;
+  grid-column-gap: 15px;
+  grid-row-gap: 40px;
+  grid-template-columns: repeat(5, minmax(300px, 1fr));
+`;
 
 function SearchResult() {
-
   return (
-      <>
-        <h1>SearchResult</h1>
-      </>
+    <S.SearchResult>
+      <Header />
+      <S.SearchResultContentWrap>
+        <ResultSummary summary="300개 이상의 숙소 · 6월 15일 - 6월 18일 · 게스트 1명" />
+        <Caution
+          imageSrc="http://dev-angelo.dlinkddns.com/caution.gif"
+          title="예약에 앞서 여행 제한 사항을 확인하세요."
+          description="에어비앤비 커뮤니티의 건강과 안전이 최우선입니다. 정부 지침을 준수하고 꼭 필요한 경우에만 여행하실 것을 부탁드립니다."
+        />
+        <S.AccomodationCardGrid>
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+          <AccomodationCard
+            src="https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg"
+            typeInfo="개인실 · 침대 0개"
+            title="ONe Bedroom * Long term * Short term * Every Term"
+            chargePerDay="13,907원/1박"
+            totalCharge="총 요금: 55,629원"
+          />
+        </S.AccomodationCardGrid>
+      </S.SearchResultContentWrap>
+    </S.SearchResult>
   );
 }
 
