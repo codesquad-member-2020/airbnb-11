c65326727ce3cfb3236d513b5dd1f53b8f99920c
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-28 23:04:10 +0900

    [FE #16] AccomodationCard 컴포넌트 작성
    Title >
    * AccomodationCard 컴포넌트 작성
    
    Description >
    * 숙소 이미지, 유형/침대 정보, 제목, 하루 숙박료, 총 요금을 타나내는
      컴포넌트 작성

--- FE/src/components/SearchResult/AccomodationCard/AccomodationCard.jsx
+++ FE/src/components/SearchResult/AccomodationCard/AccomodationCard.jsx
@@ -1,11 +1,64 @@
 import React from 'react';
+import styled, { css } from 'styled-components'
 
-function AccomodationCard() {
+const S = {};
 
+S.AccomodationCard = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+
+S.Image = styled.div`
+  width: 300px;
+  height: 200px;
+  background-image: url(${props => props.src});
+  background-size: 100% 100%;
+`;
+
+S.TypeInfo = styled.div`
+  width: 300px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  line-height: 15px;
+  font-size: 16px;
+`;
+
+S.Title = styled.div`
+  width: 300px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  line-height: 15px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 16px;
+`;
+
+S.ChargePerDay = styled.div`
+  width: 300px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  line-height: 15px;
+  font-size: 16px;
+`;
+
+S.TotalCharge = styled.div`
+  width: 300px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  line-height: 15px;
+  font-size: 14px;
+`;
+
+function AccomodationCard(props) {
   return (
-      <>
-        <h1>AccomodationCard</h1>
-      </>
+      <S.AccomodationCard>
+        <S.Image src={props.src} />
+        <S.TypeInfo>{props.typeInfo}</S.TypeInfo>
+        <S.Title>{props.title}</S.Title>
+        <S.ChargePerDay>{props.chargePerDay}</S.ChargePerDay>
+        <S.TotalCharge>{props.totalCharge}</S.TotalCharge>
+      </S.AccomodationCard>
   );
 }
 
