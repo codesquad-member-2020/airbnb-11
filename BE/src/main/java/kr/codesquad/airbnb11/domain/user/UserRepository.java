e33605d13e189547331ea86793771569675ef649
Han <quaint3@gmail.com>
2020-05-28 16:26:14 +0900

    [BE #49] refactor : 상속받는 repo 변경, 공백 수정
    향후 확장성을 위해서 repo 변경
    공백 수정

--- BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
@@ -1,10 +1,9 @@
 package kr.codesquad.airbnb11.domain.user;
 
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
-
 }
