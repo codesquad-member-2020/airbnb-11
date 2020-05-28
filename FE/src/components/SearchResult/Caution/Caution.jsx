85c9e84cfc8d8cc149fd67f9c942d28f47d975a6
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-28 23:52:13 +0900

    [FE #16] feat: Caution Component 작성
    Title >
    * Caution Component 작성
    
    Description >
    * 안내사항을 나타내는 컴포넌트 작성

--- FE/src/components/SearchResult/Caution/Caution.jsx
+++ FE/src/components/SearchResult/Caution/Caution.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import styled from "styled-components";
 
-function Caution() {
+const S = {};
 
+S.Caution = styled.div`
+  width: 1600px;
+  height: 40px;
+  display: relative;
+  margin: 0 auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+S.Image = styled.div`
+  float: left;
+  width: 40px;
+  height: 40px;
+  background-image: url(${props => props.imageSrc});
+  background-size: 100% 100%;
+`;
+
+S.Title = styled.div`
+  float: left;
+  line-height: 40px;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+S.Description = styled.div`
+  float: left;
+  font-size: 15px;
+  line-height: 40px;
+`;
+
+function Caution(props) {
   return (
-      <>
-        <h1>Caution</h1>
-      </>
+      <S.Caution>
+        <S.Image imageSrc={props.imageSrc} />
+        <S.Title>{props.title}</S.Title>
+        <S.Description>{props.description}</S.Description>
+      </S.Caution>
   );
 }
 
