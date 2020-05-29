f1a5f1af4c02b2aea5ca0f89e9491a9512984e04
IdionKim <ksundong@gmail.com>
2020-05-29 14:36:08 +0900

    [BE #68] feat: 예약 요청 API 주소 생성

--- BE/src/main/java/kr/codesquad/airbnb11/controller/RoomRestController.java
+++ BE/src/main/java/kr/codesquad/airbnb11/controller/RoomRestController.java
@@ -1,5 +1,7 @@
 package kr.codesquad.airbnb11.controller;
 
+import kr.codesquad.airbnb11.common.error.ApiResult;
+import kr.codesquad.airbnb11.controller.request.ReservationRequest;
 import kr.codesquad.airbnb11.controller.request.SearchRequest;
 import kr.codesquad.airbnb11.controller.response.SearchResponse;
 import kr.codesquad.airbnb11.domain.room.RoomDAO;
@@ -7,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +34,12 @@ public class RoomRestController {
         roomDAO.selectRoomsWithSearchParams(searchRequest));
     log.debug("searchResponse: {}", searchResponse);
     return searchResponse;
+  }
+
+  @PostMapping("/reservation")
+  public ApiResult<String> reserveRoom(@RequestBody ReservationRequest reservationRequest) {
+    log.debug("예약 요청 객체: {}", reservationRequest);
+
+    throw new RuntimeException("미구현");
   }
 }
