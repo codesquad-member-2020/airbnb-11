7538e5d6eb8666a639efc88f2ffe6afccb6d756a
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-29 10:51:58 +0900

    [FE #16] search result 에 대한 mockData 추가
    Title >
    * search result 에 대한 mockData 추가
    
    Description >
    * search result 에 렌더링 할 숙박 정보 mockData 추가

---
+++ FE/src/mockData/searchResult.js
@@ -0,0 +1,75 @@
+const searchResult = 
+[
+  {
+    src: "https://a0.muscache.com/im/pictures/2e8af4e2-480c-4dac-880d-a3d3dda13215.jpg",
+    typeInfo: "개인실 · 침대 1개",
+    title: "One Bedroom * Long term * Short term * Every Term",
+    chargePerDay: "13,620원/1박",
+    totalCharge: "총 요금: 204,294원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/85951fc5-e7b0-4570-aa8a-b376af6aba42.jpg",
+    typeInfo: "아파트 · 침대 1개",
+    title: "★ GLORY APARTMENT ★  당신의 휴식 아름다운 추억을 남겨드리겠습니다",
+    chargePerDay: "12,710원/1박",
+    totalCharge: "총 요금: 190,652원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/a140dfb2-ccdd-48d7-a815-94e7fbf773fd.jpg",
+    typeInfo: "아파트 · 침대 1개",
+    title: "Nice Nest - Studio",
+    chargePerDay: "15,910원/1박",
+    totalCharge: "총 요금: 238,654원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/c4970290-f259-47dd-b6f2-dfa6fcb77ce3.jpg",
+    typeInfo: "아파트 전체 · 침대 1개",
+    title: "★라이프스타일 스튜디오★ 및 요가 매트 | 해변 5분 거리",
+    chargePerDay: "24,017원/1박",
+    totalCharge: "총 요금: 360,258원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/aefea3f9-fe7a-4359-a8e6-3d3b9da6178e.jpg",
+    typeInfo: "개인실 · 침대 1개",
+    title: "★Vintage Studio★Windy Balcony | 2min to Beach | 2B",
+    chargePerDay: "14,517원/1박",
+    totalCharge: "총 요금: 217,753원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/a98004aa-b623-42c8-857b-a1cb0c4359da.jpg",
+    typeInfo: "개인실 · 침대 1개",
+    title: "*theNAVIS* Sunny Studio 5mins to beach",
+    chargePerDay: "15,136원/1박",
+    totalCharge: "총 요금: 227,040원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/82422546-b19b-4d44-8e41-6bb26575ed8d.jpg",
+    typeInfo: "아파트 · 침대 1개",
+    title: "@20%  Monthly discount/ Best Location_ Cozystudio",
+    chargePerDay: "15,644원/1박",
+    totalCharge: "총 요금: 234,655원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/dab43242-3ea3-43f2-8526-4d8cd2c8def6.jpg",
+    typeInfo: "아파트 · 침대 1개",
+    title: "AGFA - A home away from home, near dragon bridge",
+    chargePerDay: "12,628원/1박",
+    totalCharge: "총 요금: 189,414원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/abe3b27c-6cf9-426d-ba18-0d65b3abf548.jpg",
+    typeInfo: "아파트 전체 · 침대 1개",
+    title: "TRIPLE Studio Apartment Khue My Dong - R101",
+    chargePerDay: "18,322원/1박",
+    totalCharge: "총 요금: 274,836원",
+  },
+  {
+    src: "https://a0.muscache.com/im/pictures/37934069-e1d1-4673-b078-89a55256d869.jpg",
+    typeInfo: "개인실 · 침대 1개",
+    title: "Enjoy the luxury 1bedroom apartment at MyKhe Beach",
+    chargePerDay: "14,280원/1박",
+    totalCharge: "총 요금: 214,193원",
+  }
+]
+
+export default searchResult;
