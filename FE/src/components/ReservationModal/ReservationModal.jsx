564a043b26df6dacfc7254a62d0210ebe8e422d2
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-29 16:23:17 +0900

    [FE #16] modify: 임시적으로 alert 이 표시되도록 수정
    Title >
    * 일시적으로 alert 이 표시되도록 수정

--- FE/src/components/ReservationModal/ReservationModal.jsx
+++ FE/src/components/ReservationModal/ReservationModal.jsx
@@ -72,10 +72,7 @@ function ReservationModal({ history }) {
   }
 
   function onReservationButtonClick() {
-    var url = "popup.html";
-    var name = "popup test";
-    var option = "width = 500, height = 500, top = 100, left = 200, location = no"
-    window.open(url, name, option);
+    alert("예약이 완료되었습니다.")
     history.goBack();
   }
 
