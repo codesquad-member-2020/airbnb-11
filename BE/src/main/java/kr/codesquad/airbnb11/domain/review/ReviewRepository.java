d063418c94381634a9d51726643c8e7c952a0019
Han <quaint3@gmail.com>
2020-05-29 22:30:55 +0900

    [BE #77] refactor : Paging Repo로 변경
    추후 확장성 고려

--- BE/src/main/java/kr/codesquad/airbnb11/domain/review/ReviewRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/review/ReviewRepository.java
@@ -1,7 +1,7 @@
 package kr.codesquad.airbnb11.domain.review;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ReviewRepository extends CrudRepository<Review, Integer> {
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Integer> {
 
 }
