cfa7fd3d02247bc089e9d99bddca2164b6170434
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-29 15:44:17 +0900

    [FE #16] 라우팅 구조 수정
    Title >
    * 라우팅 구조 수정
    
    Description >
    * searchresult 이하가 중첩 라우팅이 되도록 수정

--- FE/src/routers/AppRouter.jsx
+++ FE/src/routers/AppRouter.jsx
@@ -6,14 +6,15 @@ import {
 } from "react-router-dom";
 
 import Main from 'Components/Main/Main';
-import SearchResult from 'Components/SearchResult/SearchResult'
+//import SearchResult from 'Components/SearchResult/SearchResult'
+import SearchResultRouter from 'Routers/SearchResultRouter'
 
 function AppRouter() {
   return (
     <Router>
       <Switch>
         <Route exact={true} path="/" component={Main} />
-        <Route path="/searchresult" component={SearchResult} />
+        <Route path="/searchresult" component={SearchResultRouter} />
       </Switch>
     </Router>
   );
