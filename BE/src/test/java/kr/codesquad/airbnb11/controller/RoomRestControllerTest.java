198d4ddcc9c2e925b8991b20afe7bac4af569423
IdionKim <ksundong@gmail.com>
2020-05-30 12:32:50 +0900

    [BE #48] test: 실패하는 테스트 생성

---
+++ BE/src/test/java/kr/codesquad/airbnb11/controller/RoomRestControllerTest.java
@@ -0,0 +1,22 @@
+package kr.codesquad.airbnb11.controller;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RoomRestControllerTest {
+
+  @Test
+  @DisplayName("rooms search API test")
+  void roomsSearchApiTest() {
+    fail("Not Implemented");
+  }
+
+  @Test
+  @DisplayName("rooms reserve API test")
+  void roomsReserveApiTest() {
+    fail("Not Implemented");
+  }
+
+}
