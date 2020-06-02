7e45b026e0103530c72ac81e1dcf18d7696a5d10
IdionKim <ksundong@gmail.com>
2020-06-02 21:43:14 +0900

    [BE #106] feat: 예약관련 Controller 생성

---
+++ BE/src/main/java/kr/codesquad/airbnb11/controller/ReservationRestController.java
@@ -0,0 +1,15 @@
+package kr.codesquad.airbnb11.controller;
+
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Reservation")
+@RequestMapping("/reservations")
+@RestController
+public class ReservationRestController {
+
+  private static final Logger log = LoggerFactory.getLogger(ReservationRestController.class);
+}
