205c1954a757f0fa1d8d44439602529f2052e811
IdionKim <ksundong@gmail.com>
2020-06-02 22:06:50 +0900

    [BE #106] feat: Reservation Upcoming API 개발

--- BE/src/main/java/kr/codesquad/airbnb11/domain/reservation/ReservationRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/reservation/ReservationRepository.java
@@ -1,7 +1,12 @@
 package kr.codesquad.airbnb11.domain.reservation;
 
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ReservationRepository extends PagingAndSortingRepository<Reservation, Integer> {
 
+  List<Reservation> findAllByGuestIdAndReservationDateGreaterThanEqualOrderByReservationDate(
+      Integer guestId,
+      LocalDate date);
 }
--- BE/src/main/java/kr/codesquad/airbnb11/service/ReservationService.java
+++ BE/src/main/java/kr/codesquad/airbnb11/service/ReservationService.java
@@ -1,6 +1,10 @@
 package kr.codesquad.airbnb11.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import kr.codesquad.airbnb11.domain.reservation.Reservation;
 import kr.codesquad.airbnb11.domain.reservation.ReservationRepository;
+import kr.codesquad.airbnb11.domain.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,5 +18,13 @@ public class ReservationService {
 
   public ReservationService(ReservationRepository reservationRepository) {
     this.reservationRepository = reservationRepository;
+  }
+
+  public List<Reservation> findUsersReservation(User user) {
+    log.debug("사용자의 Id: {}", user.getId());
+
+    return reservationRepository
+        .findAllByGuestIdAndReservationDateGreaterThanEqualOrderByReservationDate(user.getId(),
+            LocalDate.now());
   }
 }
