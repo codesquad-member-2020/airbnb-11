691dfb39c07ead7e5e3c24c0b137e36154b1b8d3
Han <quaint3@gmail.com>
2020-06-02 15:54:21 +0900

    [BE #77] refactor : review 제거
    room에 field로 추가하고자 삭제함

--- BE/src/main/java/kr/codesquad/airbnb11/domain/review/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.codesquad.airbnb11.domain.review;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ReviewRepository extends PagingAndSortingRepository<Review, Integer> {
-
-}
