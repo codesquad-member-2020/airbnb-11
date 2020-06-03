e6a2297863d4982aafee3ebb73708d20d36f0202
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-03 12:21:19 +0900

    [FE #107] feat: 지도 컴포넌트 작성
    Title >
    * 지도 컴포넌트 작성
    
    Description >
    * 검색 결과 화면 우측에 보여질 지도 컴포넌트 작성

---
+++ FE/src/components/SearchResult/Map/Map.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
+import { Map as LeafMap, TileLayer, Marker, Popup } from "react-leaflet";
+
+const S = {};
+S.Map = styled.div`
+  position: fixed;
+  left: 50%;
+  margin-top: 100px;
+  width: 780px;
+  height: 100%;
+  float: right;
+  z-index: 10;
+
+  .leaflet-control-attribution {
+    display: none;
+  }
+
+  .leaflet-container {
+    height: 100%;
+    width: 780px;
+  }
+
+  .leaflet-div-icon {
+    background: transparent;
+    border: none;
+  }
+`;
+
+function Map() {
+  const state = {
+    center: [51.505, -0.091],
+    zoom: 13,
+  };
+
+  const state2 = {
+    center: [51.510, -0.091],
+    zoom: 13,
+  };
+
+  return (
+      <S.Map>
+      <LeafMap center={state.center} zoom={state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={state.center}>
+          <Popup>
+            {/* <RoomInfo color="red">abc</RoomInfo> */}
+          </Popup>
+        </Marker>
+        <Marker position={state2.center}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </LeafMap>
+      </S.Map>
+  );
+}
+
+export default Map;
