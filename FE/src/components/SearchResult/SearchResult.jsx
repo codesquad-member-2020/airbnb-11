ba4032472f6a0c4bb73279edf63fd69645a24a26
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-03 12:22:22 +0900

    [FE #107] feat: 지도 컴포넌트가 렌더 되도록 개발
    Title >
    * 지도 컴포넌트가 렌더링 되도록 개발
    
    Description >
    * 화면 우측에 지도가 보이도록 수정

--- FE/src/components/SearchResult/SearchResult.jsx
+++ FE/src/components/SearchResult/SearchResult.jsx
@@ -5,28 +5,41 @@ import AccomodationCard from "Components/SearchResult/AccomodationCard/Accomodat
 import Caution from "Components/SearchResult/Caution/Caution";
 import ResultSummary from "Components/SearchResult/ResultSummary/ResultSummary";
 import Header from "Components/SearchResult/Header/Header";
-import searchResultData from "MockData/searchResult"
+import useFetch from "CustomHooks/useFetch"
+import fetchResuest from "../../utils/fetchRequest"
+import Map from "Components/SearchResult/Map/Map";
+
 
 const S = {};
 
 S.SearchResult = styled.div`
-  display: relative;
-  width: 100%;
-  margin: 0 auto;
-`;
-
-S.SearchResultContentWrap = styled.div`
-  display: relative;
+  position: relative;
   width: 1560px;
   margin: 0 auto;
 `;
 
+S.SearchResultLeft = styled.div`
+  position: relative;
+  margin-top: 100px;
+  width: 780px;
+  float: left;
+  overflow: hidden;
+`;
+
+S.SearchResultContentWrap = styled.div`
+  position: relative;
+  width: 780px;
+  margin: 0 auto;
+`;
+
 S.AccomodationCardGrid = styled.div`
+  width: 100%;
   display: grid;
+  margin-top: 50px;
   grid-template-rows: 300px;
   grid-column-gap: 15px;
   grid-row-gap: 40px;
-  grid-template-columns: repeat(5, minmax(300px, 1fr));
+  grid-template-columns: repeat(2, minmax(300px, 1fr));
 `;
 
 S.LoadingImage = styled.div`
@@ -39,45 +52,62 @@ S.LoadingImage = styled.div`
   margin-bottom: 200px;
 `;
 
+
 function SearchResult({ history }) {
-  const [searchResult, setSearchResult] = useState();
+  const [searchResult, setSearchResult] = useState(undefined);
 
   function onAccomodationCardClick() {
     history.push('/searchresult/reservationmodal');
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      setSearchResult(searchResultData);
-    }, 2000);
+    const url = "http://13.124.223.191/api/rooms/search?adult=1&checkIn=2020-07-01&checkOut=2020-07-02&parameterMap=%7B%7D";
+
+    fetchResuest(url, "GET")
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        setSearchResult(data);
+      });
   }, []);
 
   return (
     <S.SearchResult>
       <Header />
+      {!searchResult && <S.LoadingImage />}
+      <S.SearchResultLeft>
       <S.SearchResultContentWrap>
-        <ResultSummary summary="300개 이상의 숙소 · 6월 15일 - 6월 18일 · 게스트 1명" />
+        {searchResult && (
+          <ResultSummary
+            summary={
+              searchResult.roomsCount +
+              "개의 숙소" +
+              " · " +
+              "6월 15일 - 6월 18일 · 게스트 1명"
+            }
+          />
+        )}
         <Caution
           imageSrc="http://dev-angelo.dlinkddns.com/caution.gif"
           title="예약에 앞서 여행 제한 사항을 확인하세요."
           description="에어비앤비 커뮤니티의 건강과 안전이 최우선입니다. 정부 지침을 준수하고 꼭 필요한 경우에만 여행하실 것을 부탁드립니다."
         />
-        {!searchResult && <S.LoadingImage/>}
         <S.AccomodationCardGrid>
           {searchResult &&
-            searchResult.map((data, index) => (
+            searchResult.rooms.map((data, index) => (
               <AccomodationCard
                 onClick={onAccomodationCardClick}
-                key={index}
-                src={data.src}
+                key={data.id}
+                src={data.mainImage}
                 title={data.title}
-                typeInfo={data.typeInfo}
-                chargePerDay={data.chargePerDay}
+                chargePerDay={"₩" + data.dailyPrice.toLocaleString() + "원"}
                 totalCharge={data.totalCharge}
               />
             ))}
         </S.AccomodationCardGrid>
       </S.SearchResultContentWrap>
+      </S.SearchResultLeft>
+      {searchResult && <Map />}
     </S.SearchResult>
   );
 }
