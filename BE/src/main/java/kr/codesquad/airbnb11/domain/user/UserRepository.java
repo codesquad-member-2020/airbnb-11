628e94d1977e605a854fc592b774f31c1c89ce70
Han <quaint3@gmail.com>
2020-06-04 18:53:18 +0900

    [BE #99] feat : Repository 어노테이션 추가

--- BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
@@ -2,7 +2,9 @@ package kr.codesquad.airbnb11.domain.user;
 
 import java.util.Optional;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
