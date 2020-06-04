2caea79277224a42fb4a440b645266e914a0f870
Han <quaint3@gmail.com>
2020-06-04 17:54:34 +0900

    [BE #99] feat : 인터셉터 설정 추가

--- BE/src/main/java/kr/codesquad/airbnb11/common/config/WebConfig.java
+++ BE/src/main/java/kr/codesquad/airbnb11/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.util.List;
 import kr.codesquad.airbnb11.common.security.GithubKey;
+import kr.codesquad.airbnb11.controller.ReservationInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -31,6 +33,11 @@ public class WebConfig implements WebMvcConfigurer {
     return new RestTemplate();
   }
 
+  @Bean
+  public ReservationInterceptor reservationInterceptor() {
+    return new ReservationInterceptor();
+  }
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
@@ -48,5 +55,11 @@ public class WebConfig implements WebMvcConfigurer {
         .findFirst()
         .ifPresent(converter -> ((MappingJackson2HttpMessageConverter) converter)
             .setDefaultCharset(UTF_8));
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(reservationInterceptor())
+        .addPathPatterns("/reservations/**");
   }
 }
