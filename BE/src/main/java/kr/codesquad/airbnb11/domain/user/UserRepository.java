ff407a44bd07f428c4e917ae197403dd4d63cd44
Han <quaint3@gmail.com>
2020-06-04 22:28:28 +0900

    [BE #99] fix : 어노테이션 삭제
    필요없음.

--- BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
+++ BE/src/main/java/kr/codesquad/airbnb11/domain/user/UserRepository.java
@@ -2,9 +2,7 @@ package kr.codesquad.airbnb11.domain.user;
 
 import java.util.Optional;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
